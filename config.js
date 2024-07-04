068c817bca355604b4a9f3126ae089f8bc5f1fa8
Immanence <120009219+immanencer@users.noreply.github.com>
2024-07-04 15:50:05 +0000

    default port 3000

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 export const POLL_INTERVAL = 2000;
-export const TASKS_API = 'http://localhost:3030/ai/tasks';
-export const ENQUEUE_API = 'http://localhost:3030/discord/enqueue';
-export const MESSAGES_API = 'http://localhost:3030/discord/messages';
-export const LOCATIONS_API = 'http://localhost:3030/discord/locations';
-export const AVATARS_API = 'http://localhost:3030/avatars';
+export const PORT = process.env.PORT || 3000;
+export const TASKS_API = `http://localhost:${PORT}/ai/tasks`;
+export const ENQUEUE_API = `http://localhost:${PORT}/discord/enqueue`;
+export const MESSAGES_API = `http://localhost:${PORT}/discord/messages`;
+export const LOCATIONS_API = `http://localhost:${PORT}/discord/locations`;
+export const AVATARS_API = `http://localhost:${PORT}/avatars`;
 
